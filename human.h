17077c9f2a757efb406ba4257686b0634102f2ca
Russell Ruud <rruud@rosemary.umw.edu>
2013-05-24 10:00:36 -0400

    committed all

--- human.h
+++ human.h
@@ -1,5 +1,18 @@
 class human
 {
 public:
+	human();
+	void earnIncome(float);
+	void consume();
+	void haveChildren();
+	void die();
+	~human();
 private:
+	float mps;
+	float salary;
+	vector<float> cons;
+	float savings;
+	int age;
+	human *parent;
+	human *children;
 };
