5fc90e4bb44827dba480f319bdf94edc4a4ba92b
Stephen <stephen@umw.edu>
2013-06-10 13:36:05 -0400

    Refactored the transactWith() method, and enabled only super-sat trades.

--- Human.cpp
+++ Human.cpp
@@ -10,7 +10,7 @@
 #include<AgentId.h>
 #include<SharedContext.h>
 
-//using namespace std; 
+using namespace std; 
 
 void Human::tradeWithRandomAgents()
 {
@@ -38,9 +38,22 @@ std::ostream & operator<<(std::ostream & os, const Human &h) {
         " Salary " << h.salary << " Make " << h.producedCommodity << " mps " << h.mps << std::endl << "[";
 	for(int i=0; i<Commodity::NUM_COMM; i++)
 		{
-			os << h.commoditiesHeld[i] << ", ";
+            CommodityStatus cs = h.checkStatus(i);
+            switch (cs) {
+            case BLOATED:
+                os << "*";
+                break;
+            case DEFICIENT:
+                os << "?";
+                break;
+            }
+			os << h.commoditiesHeld[i];
+            if (i<Commodity::NUM_COMM-1) {
+                os  << ", ";
+            }
 		}
 		os << "]" << std::endl;
+/*
 	for(int i=0; i<Commodity::NUM_COMM; i++)
 		{
 			os << h.minThreshold[i] << ", ";
@@ -51,6 +64,7 @@ std::ostream & operator<<(std::ostream & os, const Human &h) {
 			os << h.maxThreshold[i] << ", ";
 		}
 		os << "]" << std::endl << std::endl;
+*/
     return os;
 }
 
@@ -148,6 +162,9 @@ void Human::considerHavingAChild()
 }
 
 
+// Return the number of the commodity greater than or equal to x that we are
+// deficient in. If we are not deficient in any, return NUM_COMM (which is an
+// illegal commodity number.)
 int Human::findNextDeficientCommodityStartingAt(int x)
 {
     for(int i=x; i<Commodity::NUM_COMM; i++)
@@ -157,12 +174,12 @@ int Human::findNextDeficientCommodityStartingAt(int x)
             return i;
         }
     }
-    return -1;
+    return Commodity::NUM_COMM;
 }
 
-CommodityStatus Human::checkStatus(int commodityNum)
+CommodityStatus Human::checkStatus(int commodityNum) const
 {
-    if(commoditiesHeld[commodityNum]<=minThreshold[commodityNum])
+    if(commoditiesHeld[commodityNum]<minThreshold[commodityNum])
     {
         return DEFICIENT;
     }
@@ -199,6 +216,7 @@ void Human::trade(int comm1Num, int comm2Num,
     swap(change, B, comm1Num, comm2Num);
 }
 
+/*
 bool Human::middle()
 {
 	for(int i=0; i<NUM_COMM; i++)
@@ -210,64 +228,97 @@ bool Human::middle()
 	}
 	return true;
 }
+*/
 
 void Human::transactWith(Human& other)
 {
     // 1. Make super-satisfiable trades where possible.
+    makeSuperSatisfiableTradesWith(other);
+
+    // 2. Make half-super-satisfiable trades where possible.
+    makeHalfSuperSatisfiableTradesWith(other);
+
+    // 3. Future: make other kinds of half-super-satisfiable trades where
+    // possible.
+    // Future: make ordinary-satisfiable trades happen.
+    makeOrdinarySatisfiableTradesWith(other);
+}
+
+
+void Human::makeSuperSatisfiableTradesWith(Human& other)
+{
+cout << "Looking for super trades between " << getId() << " and " <<
+other.getId() << "..." << endl;
 	int check=0;
+
+    // Go through all my commodities, searching for ones I'm deficient in.
     for(int i=0; i<Commodity::NUM_COMM; i++)
     {
-        int alow= findNextDeficientCommodityStartingAt(i);
-        if(alow!=-1 && other.checkStatus(alow)==BLOATED)
+        i= findNextDeficientCommodityStartingAt(i);
+cout << "My next deficient commodity is " << i << "..." << endl;
+        if(i!=Commodity::NUM_COMM && other.checkStatus(i)==BLOATED)
         {
+            // Okay, this is now true: I am deficient in commodity i, and 
+            // the RHS is bloated in that commodity. So we've "halfway" found 
+            // a possible super-satisfiable trade.
+cout << "Verify: I am deficient in commodity " << i << ", and RHS is bloated in commodity " <<  i << ":" << endl;
+cout << "Me: " << *this << endl;
+cout << "RHS: " << other << endl;
+        
 			//std::cout<<"Found compatible low\n";
             for(int j=0; j<Commodity::NUM_COMM; j++)
             {
-                int blow=other.findNextDeficientCommodityStartingAt(j);
-				if(blow==-1)
+                j=other.findNextDeficientCommodityStartingAt(j);
+				if(j==Commodity::NUM_COMM)
 				{
-					continue;
+                    // Well, bummer. The RHS is not deficient in anything,
+                    // so there are no super-satisfiable trades possible.
+cout << "Verify: the RHS is not deficient in anything:" << endl;
+cout << "RHS: " << other << endl;
+        
+cout << "done!" << endl;
+					return;
 				}
-                if( checkStatus(blow)==BLOATED)
+                if(checkStatus(j)==BLOATED && checkStatus(i)==DEFICIENT)
                 {
-                    // Do actual trade! A has an excess of blow and B has an
-                    // excess of alow, so trade those.
-					std::cout<<"Perfect: Sold good "<< blow <<" and bought good "<<alow<<" ";
-                    trade(alow,
-                          blow,
-                          minThreshold[alow]- commoditiesHeld[alow],
-                          other.commoditiesHeld[alow]-other.maxThreshold[alow],
-                          other.minThreshold[blow]-other.commoditiesHeld[blow],
-                          commoditiesHeld[blow]- maxThreshold[blow],
+                    // Do actual trade! A has an excess of j and B has an
+                    // excess of i, so trade those.
+					std::cout<<"Perfect: Sold good "<< j <<" and bought good "<<i<<" ";
+                    trade(i,
+                          j,
+                          minThreshold[i]- commoditiesHeld[i],
+                          other.commoditiesHeld[i]-other.maxThreshold[i],
+                          other.minThreshold[j]-other.commoditiesHeld[j],
+                          commoditiesHeld[j]- maxThreshold[j],
                           other);
-                }
-				/*std::cout<<"j is "<<j<<std::endl;
-				if(blow<9)
-				{
-					j=blow;
-				}
-				std::cout<<check<<std::endl;
-				check++;
-				if(check>12)
-				{
-					exit(1);
-				}*/
-            }
-        }
-		/*std::cout<<i<<std::endl;
-		if(alow<9)
-		{
-			i=alow;
-		}*/
-		/*check++;
-		std::cout<<check<<std::endl;
-		if(check>12)
-		{
-			exit(1);
-		}*/
-    }
 
-    // 2. Make half-super-satisfiable trades where possible.
+                    // We have now traded. However, I may still be deficient
+                    // in commodity i. If so, I need to continue to look 
+                    // for other j's to continue to super satisfy my i.
+cout << "Verify: I may still be deficient in commodity " << i << ":" << endl;
+cout << "Me: " << *this << endl;
+                }
+            }
+
+            // At this point, one of two things is true:
+            // (a) I am no longer deficient in commodity i. Yay!  ...or...
+            // (b) I am still deficient in commodity i, but I've exhausted
+            // all possibilities of trading with RHS for it.
+            // So now, continue looking for other i's I may be deficient in.
+if (checkStatus(i) == DEFICIENT) {
+cout << "This is the case Stephen wanted to check: I'm still deficient in commodity " << i << " but have run out of trading options with this guy." << endl;
+cout << "Me: " << *this << endl;
+cout << "RHS: " << other << endl;
+}
+        }
+    }
+cout << "done 2!" << endl;
+}
+
+
+void Human::makeHalfSuperSatisfiableTradesWith(Human& other)
+{
+/*
     for(int y=0; y<Commodity::NUM_COMM; y++)
     {
         int alow= findNextDeficientCommodityStartingAt(y);
@@ -297,12 +348,9 @@ void Human::transactWith(Human& other)
         }
         //y=alow;
     }
-	if(middle() && other.middle())
-	{
-		
-	}
-    // 3. Future: make other kinds of half-super-satisfiable trades where
-    // possible.
-    // Future: make ordinary-satisfiable trades happen.
+*/
 }
 
+void Human::makeOrdinarySatisfiableTradesWith(Human& other) {
+
+}
