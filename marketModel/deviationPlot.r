d47f65db85e5ca9ecebac35382e0a422e1e1862d
stephen <stephen@umw.edu>
2014-05-09 13:15:28 -0400

    Cosmetic changes to plot.

--- marketModel/deviationPlot.r
+++ marketModel/deviationPlot.r
@@ -1,8 +1,27 @@
 filenames = as.list(paste(letters[1:10],".txt",sep=""))
 lapply(filenames, function(filename) {
     omni=read.csv(filename)
-    plot(omni$avg_price,type="l",ylim=c(0,max(omni$avg_price)),main=paste(substr(filename,1,1),omni$consumption_rate[1]))
+    plot(omni$avg_price,
+		type="l",
+		ylim=c(0, max(omni$avg_price)),
+        xlab="time periods",
+		main=paste(toupper(substr(filename,1,1)), "-- Consumption rate:",
+            round(omni$consumption_rate[1]),2)
+    )
     lines(omni$sd,col="blue")
-    lines(omni$amtProduced_need,col="green")
-    legend(7,(.95*(max(omni$avg_price))), c("Avg Price","SD","Prod/Need"), lty=c(1,1,1), lwd=c(2.5,2.5,2.5),col=c("black","blue","green"))
+    # lines(omni$amtProduced_need,col="green")
+    legend(7,(.95*(max(omni$avg_price))), 
+        c("Avg Price","SD"
+            #,"Prod/Need"
+        ),
+        lty=c(1,1
+            #,1
+        ),
+        lwd=c(2.5,2.5
+            #,2.5
+        ),
+        col=c("black","blue"
+            #,"green"
+        )
+    )
 })
