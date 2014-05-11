ea72207769864f97c853e02be053952d50a2f5fd
Russell Ruud <rruudWmail.umw.edu>
2014-05-11 17:03:51 -0400

    Added shell script to run and output graph

---
+++ marketModel/run.sh
@@ -0,0 +1,10 @@
+javac *.java
+java Model 0 50 0 > results.txt
+mkdir $1
+cp results.txt $1
+cp deviationPlot.r $1
+cp splitNew.sh $1
+cd $1
+sh splitNew.sh
+Rscript deviationPlot.r
+mv Rplot.pdf $1.pdf
